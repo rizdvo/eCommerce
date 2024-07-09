855c40e001360b7c8677b7b39c99ccd18d15df6e
rizdvo <bogdanvasilev2017@gmail.com>
2024-07-09 09:49:23 +0300

    Initial commit

---
+++ src/main/java/com/vasyliev/ecommerce/ECommerceApplication.java
@@ -0,0 +1,13 @@
+package com.vasyliev.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceApplication.class, args);
+	}
+
+}
